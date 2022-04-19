079b68fbec236b028f8fcc7a5831fa55c904f8ed
Justin Morrow <morrowjustin19@gmail.com>
2022-04-18 22:40:45 -0700

    setting up basic structure

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Assignment Code
+// Assignment Code - declare all needed variables
 var generateBtn = document.querySelector("#generate");
 
+
+
+// Create functions to fill out the critera
+
+
+// Generate and write the password 
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
