ed768566efc8bd6817f3cfe69e5b59a8eea15b9c
Justin Morrow <morrowjustin19@gmail.com>
2022-04-18 22:58:21 -0700

    adding special character variables

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Assignment Code - declare all needed variables
 var generateBtn = document.querySelector("#generate");
-
-
-
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericsChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacterChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 // Create functions to fill out the critera
 // Function to select password length
 function passwordLength() {
